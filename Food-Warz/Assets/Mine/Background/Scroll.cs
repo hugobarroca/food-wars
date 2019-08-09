f8bdef5b61a19a860fb1f3a50ec48305a935008d
Hugo Barroca <hugobarroca@gmail.com>
2019-08-09 01:49:14 +0100

    Uploading the Game's Code
    First upload, some stuff might be missing, broken, in a state of extreme and total suffering or otherwise feeling unwell.

---
+++ Food-Warz/Assets/Mine/Background/Scroll.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scroll : MonoBehaviour
+{
+    public float speed = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+    GetComponent<Renderer>().material.mainTextureOffset = new Vector2(Time.time * speed, 0f);
+
+    }
+}
+    
---
+++ Food-Warz/Assets/PlayerMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+    public float horizontalMove = 0f;
+    public float runspeed = 500f;
+    bool jump = false;
+    public Animator animator;
+    public Rigidbody2D rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+        animator.SetFloat("VerticalSpeed", rigidbody.velocity.y);
+        animator.SetBool("IsGrounded", controller.m_Grounded);
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runspeed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        jump = false;
+    }
+}
